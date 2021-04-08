49c553a35078768d069181c7d969b13f5dafdb8c
Saeid <askari.saeed@gmail.com>
2021-04-08 20:53:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function main(input) {
-        if (input[0] == "F" && input == "FUZZ"){
+        if (input[0] == "F" && input[1] == "U" && input == "FUZZ"){
              throw("found the bug");
         }
 }
