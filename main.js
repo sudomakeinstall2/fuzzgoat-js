a7573f6a63e68352b6793a218743885d20ef8e19
Saeid <askari.saeed@gmail.com>
2021-04-08 21:15:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function main(input) {
         if (input[0] == "F" && input[1] == "U" && input == "FUZZ"){
-             throw("found the bug");
+             throw("CRASH");
+        }
+        if (input == "CYD"){
+             throw("ANOTHER CRASH");
         }
 }
 module.exports = { main };
