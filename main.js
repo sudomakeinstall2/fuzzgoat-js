a4299a108043a510620aba41c352b3e816473b32
Saeid <askari.saeed@gmail.com>
2021-04-10 11:32:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function main(input) {
         if (input[0] == "F" && input[1] == "U" && input[2] == "Z" && input == "FUZZ"){
              throw("CRASH");
         }
-        if (input[0] == "C" && input[1] == "Y" && input == "CYD"){
+        if (input[0] == "C" && input[1] == "Y" && input[2] == "D" && input == "CYD"){
              throw("ANOTHER CRASH");
         }
 }
